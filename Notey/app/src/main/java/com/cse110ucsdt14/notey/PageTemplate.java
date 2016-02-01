e0c211c05dd0546e57cb40facf1ebb1e593b3151
Collin Wasley <cwasley@ucsd.edu>
2016-02-01 15:16:26 -0800

    Test

--- Notey/app/src/main/java/com/cse110ucsdt14/notey/PageTemplate.java
+++ Notey/app/src/main/java/com/cse110ucsdt14/notey/PageTemplate.java
@@ -24,6 +24,7 @@ public class PageTemplate extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                // penisia
             }
         });
     }
