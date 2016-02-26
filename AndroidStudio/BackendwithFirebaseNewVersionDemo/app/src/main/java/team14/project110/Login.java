984799fe63b9db4fcaa5e0761a454e04df8ecbbd
c6ramos <c6ramos@ucsd.edu>
2016-02-26 15:27:44 -0800

    Demo for backend

---
+++ AndroidStudio/BackendwithFirebaseNewVersionDemo/app/src/main/java/team14/project110/Login.java
@@ -0,0 +1,56 @@
+package team14.project110;
+
+import java.util.Scanner;
+
+/**
+ * Created on 2/25/2016.
+ */
+public class Login {
+   /* Scanner scanner = new Scanner(System.in);  	// change to word scanner
+    String loginName = scanner.nextLine();
+    String name;
+    String email;
+    list<UserProfile> users;
+    int index = 0;
+
+    while (1) {
+        check for profile name
+        check for saved classes
+        users[index] = new UserProfile (info_from text)
+        for (saved classes) {
+            loop to add courses
+        }
+        index++;
+    }
+
+    while (1) {
+
+        for (int i = 0; i < loginName.length(); i++) {
+            if (loginName.charAt(i) == ‘@’) {
+                email = loginName.substring(i);
+                name = loginName.substring(0, i);
+            }
+        }
+
+        if ( email.equals(“@ucsd.edu”)) {
+            break;
+        }
+        else {
+            System.out.println(“Please enter a valid UCSD email.”);
+        }
+
+    }
+
+    for (int i = 0, i < users.size(); i++) {
+        if (users[i].isUser(name) {
+            start front end under previous user
+        }
+    }
+
+    UserProfile newUser = new UserProfile(name);
+
+    //add to text file
+   //start the front end under new user
+*/
+
+}
