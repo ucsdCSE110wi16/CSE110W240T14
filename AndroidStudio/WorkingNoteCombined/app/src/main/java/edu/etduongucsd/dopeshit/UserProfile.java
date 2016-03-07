e897c2684622526fa055089deeb669d7caaf8070
steven6790 <spartida6162@gmail.com>
2016-03-06 20:41:02 -0800

    upvote/flag toggler

--- AndroidStudio/WorkingNoteCombined/app/src/main/java/edu/etduongucsd/dopeshit/UserProfile.java
+++ AndroidStudio/WorkingNoteCombined/app/src/main/java/edu/etduongucsd/dopeshit/UserProfile.java
@@ -10,6 +10,8 @@ import java.util.List;
 public class UserProfile {
     String name;
     public static List<Course> myCourses = new ArrayList<Course>();
+    public List<Note> myFlags = new ArrayList<>();
+    public List<Note> myUpvotes = new ArrayList<>();
     //List<Note> myNotes;
     MyNotes myNotes = new MyNotes();
 
@@ -32,6 +34,43 @@ public class UserProfile {
     //public void addMyNote(Note note){
     //    myNotes.add(note);
     //}
+
+    //Compares a note to add or remove from Note List (either myFlags or myUpvotes)
+    public boolean containsNote( Note n, List<Note> myList) {
+        if(myList == null) {
+            return false;
+        }
+
+        for(Note currentNote : myList) {
+            if( (currentNote.dataBaseRef + currentNote.toString()).equals(n.dataBaseRef + n.toString())) {
+                return true;
+            }
+        }
+
+        return false;
+   }
+
+    //Toggle the upvotes by user, if note is in the myUpvotes list, remove it, else add it.
+    //Returns true if added, false if removed.
+    public void toggleMyUpvote(Note n) {
+        if ( containsNote( n, myUpvotes)) {
+            myUpvotes.remove(n);
+        }
+        else {
+            myUpvotes.add(n);
+        }
+    }
+
+    //Toggle the flags by user, if note is in the myFlags list, remove it, else add it.
+    //Returns true if added, false if removed
+    public void toggleMyFlags(Note n) {
+        if ( containsNote( n, myFlags)) {
+            myFlags.remove(n);
+        }
+        else {
+            myFlags.add(n);
+        }
+    }
 
     public boolean isUser(String s) {
         return name.equals(s);
